a05906542bef0e9a4db91978a5097b7f44d78116
Kai Hendry <hendry@iki.fi>
2008-07-17 10:44:55 +0100

    damn brackets

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 function log(x) {
 	logdiv = window.document.getElementById("logdiv");
 	if (parseInt(x) === 42) {
+	logdiv.innerHTML = 'PASS';
 	logdiv.setAttribute("style", "color: green;");
 	} else {
+	logdiv.innerHTML = 'FAIL';
 	logdiv.setAttribute("style", "color: red;");
 	}
-	logdiv.innerHTML += '<div>' + x.toString() + "</div>\n";}
+}
