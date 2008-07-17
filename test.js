ea348ee9381e05dc0d4787a815d5e87eed0df69a
Kai Hendry <hendry@iki.fi>
2008-07-17 10:36:03 +0100

    toString() seems to have been the bug :(

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 function log(x) {
-	var test = x;
 	logdiv = window.document.getElementById("logdiv");
-	logdiv.innerHTML += '<div>' + x.toString() + "</div>\n";}
-	if (test === '42') {
+	if (parseInt(x) === 42) {
 	logdiv.setAttribute("style", "color: green;");
 	} else {
 	logdiv.setAttribute("style", "color: red;");
 	}
-
+	logdiv.innerHTML += '<div>' + x.toString() + "</div>\n";}
